1044cb37cf97ab7f78c48c2bf85771c056f4a99e
Andyl2007 <andyflee137@gmail.com>
2023-10-20 20:12:23 -0400

    Initial Commit

---
+++ Glutenwise/GlutenwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlutenwiseApp.swift
+//  Glutenwise
+//
+//  Created by Andy on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GlutenwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
