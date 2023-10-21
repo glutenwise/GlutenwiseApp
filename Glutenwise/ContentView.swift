c71554c462d27dddf5a487c14b7516316a8b3cf7
gracexf <gracexf07@gmail.com>
2023-10-20 22:06:44 -0400

    added text & image elements onto the restaurants page
    This is not completely formatted correctly yet. The restaurant list and navigation bar need to be added next.

--- Glutenwise/ContentView.swift
+++ Glutenwise/ContentView.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+                Text("glutenwise LOGO")
+            ZStack {
+                Image("restaurant")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .scaledToFit()
+                    .position(x: 187.5, y: 110)
+//                    .overlay(Text("RESTAURANTS")
+//                        .font(.largeTitle)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(Color.white), alignment: .top)
+                VStack {
+                    Image("blueBadge")
+                    Image("greenBadge")
+                    Image("yellowBadge")
+                    Image("redBadge")
+                }
+                Text("RESTAURANTS")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .position(x: 140, y: 180)
+                Menu("Filter") {
+                    Text("BADGE")
+                    Text("PRICE")
+                    Text("DISTANCE")
+            }
+                
+            }
+            
+        }
+            
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
