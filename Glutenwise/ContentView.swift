373ba8f4a9d2cb3b7ec4b49c443a572b8fa7019f
gracexf <gracexf07@gmail.com>
2023-10-22 16:13:25 -0400

    changed top restaurant banner

--- Glutenwise/ContentView.swift
+++ Glutenwise/ContentView.swift
@@ -11,39 +11,18 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-                Text("glutenwise LOGO")
-            ZStack {
-                Image("restaurant")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaledToFit()
-                    .position(x: 187.5, y: 110)
-//                    .overlay(Text("RESTAURANTS")
-//                        .font(.largeTitle)
-//                        .fontWeight(.bold)
-//                        .foregroundColor(Color.white), alignment: .top)
-                VStack {
-                    Image("blueBadge")
-                    Image("greenBadge")
-                    Image("yellowBadge")
-                    Image("redBadge")
-                }
-                Text("RESTAURANTS")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .padding()
-                    .position(x: 140, y: 180)
-                Menu("Filter") {
-                    Text("BADGE")
-                    Text("PRICE")
-                    Text("DISTANCE")
+            Text("glutenwise LOGO")
+            Image("restaurant")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .scaledToFit()
+                .position(x: 187.5, y: 110)
+            Menu("Filter") {
+                Text("BADGE")
+                Text("PRICE")
+                Text("DISTANCE")
             }
-                
-            }
-            
         }
-            
     }
 }
 
