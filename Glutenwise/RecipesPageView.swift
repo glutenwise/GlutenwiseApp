9b12fe19879d999d32551e4e2e5f4a9a7a938b62
Andyl2007 <andyflee137@gmail.com>
2023-10-28 17:22:55 -0400

    fixed up most formatting issues on the recipes page

--- Glutenwise/RecipesPageView.swift
+++ Glutenwise/RecipesPageView.swift
@@ -4,16 +4,13 @@
 //
 //  Created by grace on 10/28/23.
 //
-
 import SwiftUI
-
 struct RecipesPageView: View {
     var body: some View {
         ScrollViewReader { proxy in
-            Text("Glutenwise")
-                .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .position(x: 175, y: 10)
+            ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+            Text("Glutenwise logo")
             ZStack {
                 Image("food1Image")
                     .resizable()
@@ -25,72 +22,65 @@ struct RecipesPageView: View {
                     .foregroundColor(Color.white)
                     .position(x: 160, y: 110)
             }
-            Text("  Ingredients")
-                .font(.title)
-                .fontWeight(.semibold)
-                .position(x:80, y:-250)
-        Text("""
-             - 1 pound chicken breast cut in strips
-             - salt to taste
-             - 1 teaspoon lemon juice
-             - 1 teaspoons smoked paprika
-             - 1.5 cups quinoa
-             - 1 tablespoon olive oil
-             - 2 cloves garlic chopped
-             - 3 cups water or a little bit less
-             - 8 ounces mushrooms sliced
-             - 1 tablespoon olive oil
-             - 1 pound asparagus cut in 2 inches
-             - 3 cloves garlic chopped
-             - bunch of fresh basil thinly sliced
-             - 1-2 tablespoon lemon juice
-""")
-        .position(x:100, y:-100)
-        Text("Instructions")
-                .font(.title)
-                .fontWeight(.semibold)
-                .position(x: 80, y: 0)
-            
-        Text("""
-1.) In a bowl marinate chicken with salt, 1 teaspoon lemon juice and paprika. Set aside.
 
-2.) In a pot heat 1 tablespoon olive oil and add
-2 cloves chopped garlic. Cook until just
-aromatic and add quinoa. Cook 1-2 minutes
-until lightly toasted. Add water. Bring to a boil. Reduce heat and simmer covered for 20
-minutes or until cooked through.
 
-3.) In a pan heat 1 tablespoon olive oil and add mushrooms in single layer. Do not crowd the
-pan and cook them in batches in you have to. Cook on each side until lightly browned and
-set aside. In the same pan heat 1 tablespoon olive oil and add chicken in single layer. Cook until lightly browned on each side and remove.
-
-4.) To same pan add asparagus and garlic.
-Add more oil if needed. Cook until asparagus
-is cooked through and lightly softened.
-
-5.) In a large bowl combine quinoa, chicken, asparagus, garlic and mushrooms, Mix. Add
-fresh basil, lemon juice and more oil if mixture is dry.
-""")
-    
-        .position(x: 185, y: 270)
-            Text("Comments")
+                Text("  Ingredients")
                     .font(.title)
                     .fontWeight(.semibold)
-                    .position(x: 80, y: 0)
-            Text("Anonymous User 191                       10/8/2023")
-            Text("Wow! This perfect recipe is both healthy and delicious. ")
-                .fontWeight(.light)
-                .font(.caption)
-                .foregroundColor(Color.gray)
-                .position(x:   175, y: 0)
+
+                Text("""
+                     - 1 pound chicken breast cut in strips
+                     - salt to taste
+                     - 1 teaspoon lemon juice
+                     - 1 teaspoon smoked paprika
+                     - 1.5 cups quinoa
+                     - 1 tablespoon olive oil
+                     - 2 cloves garlic chopped
+                     - 3 cups water or a little bit less
+                     - 8 ounces mushrooms sliced
+                     - 1 tablespoon olive oil
+                     - 1 pound asparagus cut in 2 inches
+                     - 3 cloves garlic chopped
+                     - bunch of fresh basil thinly sliced
+                     - 1-2 tablespoons lemon juice
+                """)
+                .multilineTextAlignment(.leading)
+                .padding()
+
+                Text("  Instructions")
+                    .font(.title)
+                    .fontWeight(.semibold)
+
+                Text("""
+                1.) In a bowl marinate chicken with salt, 1 teaspoon lemon juice and paprika. Set aside.
+                2.) In a pot heat 1 tablespoon olive oil and add 2 cloves chopped garlic. Cook until just aromatic and add quinoa. Cook 1-2 minutes until lightly toasted. Add water. Bring to a boil. Reduce heat and simmer covered for 20 minutes or until cooked through.
+                3.) In a pan heat 1 tablespoon olive oil and add mushrooms in a single layer. Do not crowd the pan and cook them in batches if you have to. Cook on each side until lightly browned and set aside. In the same pan heat 1 tablespoon olive oil and add chicken in a single layer. Cook until lightly browned on each side and remove.
+                4.) To the same pan add asparagus and garlic. Add more oil if needed. Cook until asparagus is cooked through and lightly softened.
+                5.) In a large bowl combine quinoa, chicken, asparagus, garlic, and mushrooms, Mix. Add fresh basil, lemon juice, and more oil if mixture is dry.
+                """)
+                .padding()
+
+                Text("  Comments")
+                    .font(.title)
+                    .fontWeight(.semibold)
+
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("  Anonymous User 191                       10/8/2023")
+
+                    Text("   Wow! This perfect recipe is both healthy and delicious.")
+                        .fontWeight(.light)
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                }
+            }
+
             
         }
         }
     }
-
-
 struct RecipesPageView_Previews: PreviewProvider {
     static var previews: some View {
         RecipesPageView()
     }
+}
 }
