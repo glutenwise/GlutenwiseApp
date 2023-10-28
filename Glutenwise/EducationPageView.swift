50d0f9f292686e09616f33b742bf2188dcb6084a
gracexf <gracexf07@gmail.com>
2023-10-28 16:01:51 -0400

    created new page titled EducationPageView

---
+++ Glutenwise/EducationPageView.swift
@@ -0,0 +1,20 @@
+//
+//  EducationPageView.swift
+//  Glutenwise
+//
+//  Created by grace on 10/28/23.
+//
+
+import SwiftUI
+
+struct EducationPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EducationPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        EducationPageView()
+    }
+}
